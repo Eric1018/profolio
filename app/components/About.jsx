924234f47e5a8f922ee12ab9670f2d3c4b3bcc9a
Eric1018 <qwer6986270@gmail.com>
2025-02-08 22:22:01 +0800

    fix: update viewport settings

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,7 @@ import { infoList, toolsData } from '@/public/asset'
 
 const About = ({isDarkMode}) => {
   return (
-    <motion.div id='about'className='w-full px-[12%] py-10 scroll-mt-20 transform scale-90'
+    <motion.div id='about'className='w-full px-[12%] py-10 scroll-mt-20 transform scale-90 sm:scale-90'
     initial={{opacity:0}}
     whileInView={{opacity:1}}
     transition={{duration:1}}
